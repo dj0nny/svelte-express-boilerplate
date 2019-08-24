c7f465e0afd08e891025722b3aa59eab35dc7f3a
dj0nny <frank96990@gmail.com>
2019-08-24 16:54:37 +0200

    edit app.js

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
 const express = require('express');
-const bodyParser = require('body-parser');
 const cors = require('cors');
+const bodyParser = require('body-parser');
 const path = require('path');
 
 const app = express();
